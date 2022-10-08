e0dea035b4c259148b6eb6a36ca6be626cb9f54b
Qacek <102095355+Qacek@users.noreply.github.com>
2022-10-08 17:25:23 +0200

    Update PLUGIN_NAME.plugin.ts

--- src/PLUGIN_NAME.plugin.ts
+++ src/PLUGIN_NAME.plugin.ts
@@ -1,5 +1,5 @@
 /**
- * @name PLUGIN_NAME
+ * @name silent
  * @version 1.0.0
  * @description nothing
  * 
@@ -7,30 +7,18 @@
  */
 
 import { BdApi } from "@bandagedbd/bdapi";
+import definePlugin from "../utils/types";
+import { Devs } from "../utils/constants";
 
-export class PLUGIN_NAME {
-
-    public getName(): string {
-        return "PLUGIN_NAME";
-    }
-
-    public getDescription(): string {
-        return "Describe the basic functions. Maybe a support server link";
-    }
-
-    public getVersion(): string {
-        return "1.0.0";
-    }
-
-    public getAuthor(): string {
-        return "CorrM";
-    }
-
-    public start(): void {
-        BdApi.alert("CORRM", "Just check");
-    }
-
-    public stop(): void {
-
-    }
-}
+export default definePlugin({
+    name: "SilentTyping",
+    authors: [Devs.Ven],
+    description: "Hide that you are typing",
+    patches: [{
+        find: "startTyping:",
+        replacement: {
+            match: /startTyping:.+?,stop/,
+            replace: "startTyping:()=>{},stop"
+        }
+    }]
+});
